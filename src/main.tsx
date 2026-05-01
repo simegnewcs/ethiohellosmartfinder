a64abbcfc696b891c61742be93918a9efe1d443a
Smegne <smegndestew2@gmail.com>
2026-05-01 11:24:33 +0300

    fix advanced ceo production

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,6 @@ import { RouterProvider } from 'react-router-dom'
 import AppRouter from './routes/AppRouter'
 import './index.css'
 
-// SEO: Update document title on route change
-const originalTitle = document.title;
-
 // Track page views for SEO (optional - for Google Analytics)
 if (import.meta.env.PROD) {
   console.log('EthioHello - Production mode');
@@ -16,4 +13,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={AppRouter} />
   </React.StrictMode>,
-);
+)
