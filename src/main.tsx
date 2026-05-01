0572d3b7ff65d9ffd745b010926593429d3b48f1
Smegne <smegndestew2@gmail.com>
2026-05-01 11:19:52 +0300

    Advance SEO

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import AppRouter from './routes/AppRouter'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+// SEO: Update document title on route change
+const originalTitle = document.title;
+
+// Track page views for SEO (optional - for Google Analytics)
+if (import.meta.env.PROD) {
+  console.log('EthioHello - Production mode');
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={AppRouter} />
+  </React.StrictMode>,
 );
